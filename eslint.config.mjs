967bf21171be76d58b436d22bceb942f15308475
Pavel Vinogradov <pavel.vinogradov@mpstats.io>
2024-11-30 20:19:35 +0300

    Добавлен пакет специальных правил

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,21 @@
 import local from 'eslint-plugin-local';
 import config from 'turmag-eslint-config';
+import specialRules from 'eslint-plugin-turmag-special-rules';
 import { aliases } from './aliases.mjs';
 
 export default [
-    { plugins: { local } },
+    {
+        plugins: {
+            specialRules,
+            local,
+        },
+    },
 
     ...config,
 
     {
         rules: {
-            'local/prefer-true-attribute-shorthand': ['error', 'always'],
+            'specialRules/prefer-true-attribute-shorthand': 'error',
             'local/add-vue-extension': ['error', { aliases }],
             'local/use-shortest-alias': ['error', { aliases }],
             'local/import-entities-by-column-or-line': ['error', { minProperties: 3 }],
