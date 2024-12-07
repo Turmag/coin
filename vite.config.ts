f65455a0c4f9ac456e9b309d5fe5edd646e83e9d
Pavel Vinogradov <pavel.vinogradov@mpstats.io>
2024-12-07 13:48:24 +0300

    Небольшие правки линтером

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,18 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }: { mode: string }) => {
-    return {
-        plugins: [vue()],
-        base: '/coin/',
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, './src'),
-                '@main': path.resolve(__dirname, './src/components/main'), 
-                '@header': path.resolve(__dirname, './src/components/header'), 
-            }, 
+export default defineConfig(({ mode }: { mode: string }) => ({
+    plugins: [vue()],
+    base: '/coin/',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@main': path.resolve(__dirname, './src/components/main'),
+            '@header': path.resolve(__dirname, './src/components/header'),
         },
-        css: {
-            modules: { generateScopedName: mode === 'development' ? '' : '[hash:base64:8]' },
-            preprocessorOptions: { scss: { api: 'modern-compiler' } },
-        },
-    };
-});
+    },
+    css: {
+        modules: { generateScopedName: mode === 'development' ? '' : '[hash:base64:8]' },
+        preprocessorOptions: { scss: { api: 'modern-compiler' } },
+    },
+}));
